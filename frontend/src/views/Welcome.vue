c045d02697651a4cd886fd1e6ecb5e5cc3997322
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-12-17 16:54:11 +0900

    update: propsの使い方
    子コンポーネントから親コンポーネントのメソッドを実行する方法も

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p v-if="isEnabled">{{ subtitle }}</p>
-    <button @dblclick="toggle">トグルする</button>
-    <p>{{text}}</p>
+    <TestComponent @toggle="toggle" message="コンポーネントにデータが渡されています"/>
+    <p v-if="isEnabled">こんにちは</p>
   </div>
 </template>
 
 <script>
+import TestComponent from '../components/TestComponent.vue'
+
 export default{
+  components:{
+    TestComponent
+  },
   data(){
     return{
       title: '初めてのVue.jsあぷり',
