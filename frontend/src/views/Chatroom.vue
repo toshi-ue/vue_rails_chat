051f023c8423ed9d864a4e1042a01784edb0850c
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-12-18 10:34:03 +0900

    update(frontend): メッセージ一覧を取得する

--- frontend/src/views/Chatroom.vue
+++ frontend/src/views/Chatroom.vue
@@ -1,14 +1,45 @@
 <template>
   <div class="container">
     <Navbar />
+    <ChatWindow :messages="messages"/>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import Navbar from '../components/Navbar'
+import ChatWindow from '../components/ChatWindow'
 
 export default {
-  components: { Navbar },
+  components: { Navbar, ChatWindow },
+  data(){
+    return{
+      messages:[],
+    }
+  },
+  methods:{
+    async getMessages(){
+      try{
+        const res = await axios.get('http://localhost:3000/messages',{
+          headers:{
+            uid: window.localStorage.getItem('uid'),
+            "access-token": window.localStorage.getItem('access-token'),
+            client:window.localStorage.getItem('client')
+          }
+        })
+
+        if(!res){
+          new Error('メッセージ一覧を取得できませんでした')
+        }
+        this.messages = res.data
+      }catch(err){
+        console.log(err)
+      }
+    }
+  },
+  mounted(){
+    this.getMessages()
+  }
 }
 </script>
 
