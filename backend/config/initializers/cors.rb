b82ae50362e228177e6d812b824d150d5ac6690e
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-12-23 21:08:48 +0900

    update: CORSのドメイン設定を追加

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'vue-rails-chat2.herokuapp.com'
 
     resource '*',
       headers: :any,
